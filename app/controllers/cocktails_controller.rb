62dda5de22fe5cdfef00112f006ba9d5cc49d186
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-02-22 15:37:55 +0000

    photo uploader done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -29,6 +29,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_param
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
