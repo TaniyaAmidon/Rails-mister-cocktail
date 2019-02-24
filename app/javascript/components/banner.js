8e6c6354fbd27812948d671bd985e5cefedce15e
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-02-24 22:14:07 +0000

    js components added

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,16 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#typed-text', {
+    strings: ["Make your favourite cocktail at home"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
+
+
+
+
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,4 +8,7 @@ import 'bootstrap';
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
 console.log('Hello World from Webpacker')
+
